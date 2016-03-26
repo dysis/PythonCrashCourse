d21f7de7256fb1dc8bc089db4183624642ee815a
dysis <dianaarruda@gmail.com>
2016-03-26 09:31:51 -0400

    Added chapter 4

---
+++ Chapter_4/chapter4_animals.py
@@ -0,0 +1,6 @@
+animals = ['cat', 'dog', 'squirrel']
+
+for animal in animals:
+    print("A " + animal.title() + " is really cute and furry!\n")
+
+print("All these animals are the best!")
---
+++ Chapter_4/chapter4_pizza.py
@@ -0,0 +1,6 @@
+pizzas = ['pizza hut', 'pizza nova', 'pizza pizza', '2 for 1']
+
+for pizza in pizzas:
+    print("I haven't had a pizza from " + pizza.title() + " in a long time!\n")
+
+print("I should get a pizza from there soon!")
