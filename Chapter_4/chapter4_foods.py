ee2a7348bb0f6f1347108f1617c39c2ae343385e
dysis <dianaarruda@gmail.com>
2016-03-27 09:55:25 -0400

    last of chapter 4 exercises

--- Chapter_4/chapter4_foods.py
+++ Chapter_4/chapter4_foods.py
@@ -1,4 +1,4 @@
-my_foods = ['pizza', 'falafel', 'carrot cake']
+my_foods = ['pizza', 'falafel', 'carrot cake', 'sushi', 'thai']
 friend_foods = my_foods[:]
 
 my_foods.append('cannoli')
@@ -9,3 +9,15 @@ print(my_foods)
 
 print("\nMy friend's favourite foods are:")
 print(friend_foods)
+
+print("The first 3 items in the list are:")
+print(my_foods[:4])
+
+print("Three items from the middle of the list are")
+print(my_foods[2:5])
+
+print("The three last items in the list are:")
+print(my_foods[-3:])
+
+for food in my_foods:
+    print("I really love, " + food + ".")
---
+++ Chapter_4/chapter4_tuples.py
@@ -0,0 +1,15 @@
+dimensions = (200, 5)
+print(dimensions[0])
+print(dimensions[1])
+
+for dimension in dimensions:
+    print(dimension)
+
+print("Original dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (400, 20)
+print("New dimensions:")
+for dimension in dimensions:
+    print(dimension)
