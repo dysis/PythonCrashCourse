9a3b696fd95146d29d7bea982a6dd48dfb1d7e64
dysis <dianaarruda@gmail.com>
2016-03-28 22:57:35 -0400

    additional Chapter 5

---
+++ Chapter_5/chapter5_assignments.py
@@ -0,0 +1,7 @@
+alien_colour = 'green'
+
+if alien_colour == 'green':
+    print("My alien is green! You got 5 points")
+
+if alien_colour == 'blue':
+    print("My alien is blue! You got 5 points")
