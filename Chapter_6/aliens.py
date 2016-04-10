bae7de57501e76311701b2fa3644059e01bd3523
dysis <dianaarruda@gmail.com>
2016-04-09 20:42:45 -0400

    Chapter 6

---
+++ Chapter_6/aliens.py
@@ -0,0 +1,32 @@
+alien_0 = {'color' : 'green', 'points' : 5}
+alien_1 = {'color' : 'yellow', 'points' : 10}
+alien_2 = {'color' : 'red', 'points' : 15}
+
+aliens = [alien_0, alien_1, alien_2]
+
+for alien in aliens:
+    print(alien)
+
+
+aliens = []
+
+# Make 30 green aliens.
+for alien_number in range(30):
+    new_alien = {'color' : 'green', 'points' : 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+
+# Change alien colors
+for alien in aliens[0:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['speed'] = 'medium'
+        alien['points'] = 10
+
+# Show the first 5 aliens
+for alien in aliens[:5]:
+    print(alien)
+
+print("...")
+
+# Show how many aliens have been created
+print("\nTotal number of aliens: " + str(len(aliens)))
--- Chapter_6/favorite_languages.py
+++ Chapter_6/favorite_languages.py
@@ -9,3 +9,43 @@ print ("Sarah's favorite language is " +
        favorite_languages['sarah'].title() +
        ".")
 
+for name, language in favorite_languages.items():
+    print(name.title() + "'s favorite language is " +
+          language.title() + ".")
+
+for name in favorite_languages.keys():
+    print(name.title())
+
+print("\nNow starting friends section.")
+
+friends = ['phil', 'sarah']
+
+for name in favorite_languages.keys():
+    print(name.title())
+
+    if name in friends:
+        print(" Hi " + name.title() +
+              ", I see your favorite language is " +
+              favorite_languages[name].title() + "!")
+
+print("\n")
+
+if 'erin' not in favorite_languages.keys():
+    print("Erin, please take our poll!")
+
+print("\n")
+
+for name in sorted(favorite_languages.keys()):
+    print(name.title() + ", thank you for taking the poll.")
+
+print("\n")
+
+print("The following languages have been mentioned:")
+for language in favorite_languages.values():
+    print(language.title())
+
+print("\n")
+
+print("The following languages have been mentioned:")
+for language in set(favorite_languages.values()):
+    print(language.title())
