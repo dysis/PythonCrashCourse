0b072de3045a988c96ba2e675f073e39745b28a7
dysis <dianaarruda@gmail.com>
2016-04-10 16:43:03 -0400

    Chapter 7

---
+++ Chapter_7/greeter.py
@@ -0,0 +1,10 @@
+name = input("Please enter your name: ")
+print("Hello there, " + name + "!")
+
+prompt = "If you tell us who you are, we can personalize the messages you see."
+prompt += "\nWhat is your first name? "
+
+name = input(prompt)
+print("\nHello, " + name + "!")
+
+age = input("How old are you ? ")
